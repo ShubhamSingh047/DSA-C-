a2d79a3fe2906081c63e659aef2bff34adaf59a8
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-02-18 02:54:02 +0530

    first indices

---
+++ Recursion/firstIndex.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int firstIndex(int arr[], int x, int s){
+    if(s==0){
+        return -1;
+    }
+    int sres=firstIndex(arr, x, s-1);
+    if(sres==-1){
+        if(arr[s]==x)return s;
+        else return -1;
+    }else{
+        return sres;
+    }
+}
+
+int main(){
+    int arr[]={12,3,4,2,1};
+    int s=sizeof(arr)/sizeof(arr[0]);
+    cout<<firstIndex(arr, 1, s);
+}

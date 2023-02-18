3f15054b98d01088d948474615fe6d7a673f523b
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-02-18 22:38:24 +0530

    all indeices

--- Recursion/AllIndicesOfArray.cpp
+++ Recursion/AllIndicesOfArray.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 int Solution(int arr[], int s, int x, int out[]){
-    if(s==0)return 0;
-    int res=(arr,s-1,x,out);
+    if(s<0)return 0;
+    //as size will go below zero it eill return 0;
+    int res=Solution(arr,s-1,x,out); 
+    //so we will check for arr[0]==x and the push the size into out arr;
     if(arr[s]==x){
         out[res]=s;
         res++;
-        return res;
-    }else return res;
+    }
+    return res; 
 }
 
 int main(){
-    int arr[]={1,2,3,1,4,9};
+    int arr[]={1,2,3,1,4,9,1};
     int s=sizeof(arr)/sizeof(arr[0]);
     int out[2];
-    Solution(arr,s,1,out);
+    cout<<Solution(arr,s,1,out)<<endl;
+    for(auto i:out)cout<<i;
 }

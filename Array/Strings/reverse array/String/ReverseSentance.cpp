956c04b22692a8a83482eb40de678a63b5cdebfe
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-02-25 19:43:38 +0530

    updated reverse string

--- Array/Strings/reverse array/String/ReverseSentance.cpp
+++ Array/Strings/reverse array/String/ReverseSentance.cpp
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void reverse(string str,int l, int r ){
+void reverse(string& str,int l, int r ){
     while(l<=r){
         swap(str[l],str[r]);
         l++;
         r--;
     }
 }
-void reverseSentance(string str){
-    // str.insert(str.end(), ' ');
-    // int j=0;
-    // for(int i=0;i<str.length(); i++){
-    //     if(str[i]==' '){
-    //        reverse(str, j,i-1); 
-    //     }
-    //     j=i+1;
-    // }
-    // str.pop_back();
+void reverseSentance(string& str){
+    str.insert(str.end(), ' ');
+    int j=0;
+    for(int i=0;i<str.length(); i++){
+        if(str[i]==' '){
+           reverse(str, j,i-1); 
+           j=i+1;
+        }
+        
+    }
+    str.pop_back();
     reverse(str, 0, str.length() - 1);
 }
 

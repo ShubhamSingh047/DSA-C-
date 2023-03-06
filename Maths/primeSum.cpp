4bb45b662cb31a9bbc7ab851f8a7bab450cd8315
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-03-06 12:51:25 +0530

    Prime Sum:-  return Yes if sum pairs is prime

---
+++ Maths/primeSum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Given a number N find if it can be expressed as sum of 
+two prime number ?
+Input:34
+ans "yes"
+34 can be expressed as sum of two prime number
+*/
+
+
+bool checkPrime(int x){
+    if(x<2)return false;
+    for(int i=2; i*i<x; i++){
+        if(x%i==0)return false;
+    }
+    return true;
+}
+
+string bruteForce(int x){
+    int temp=0;
+    if(checkPrime(x-2))return "Yes";
+    for(int i=3; i*i<=x; i+=2){
+        temp=x-i;
+        if(checkPrime(temp) && checkPrime(i)) return "Yes";
+    }
+    return "No";
+}
+
+int main(){
+    int x;
+    cin>>x;
+    cout<<bruteForce(x);
+}

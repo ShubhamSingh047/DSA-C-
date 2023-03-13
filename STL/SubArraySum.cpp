e132c69e83ece63bb489d47b5b247917cb8a0627
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-03-13 17:10:59 +0530

    subarray_sum for given num

--- STL/SubArraySum.cpp
+++ STL/SubArraySum.cpp
@@ -18,9 +18,25 @@ bool bruteforce(int arr[],int n){
     return false;
 }
 
+bool optimumApproch(int arr[], int n){
+    unordered_set<int>s;
+    int pre_sum=0;
+    for(int i=0; i<n; i++){
+        pre_sum+=arr[i]
+        if(s.find(pre_sum)!=s.end()){
+            return true;
+        }
+        if(pre_sum==0){
+            return true;
+        }
+        s.insert(pre_sum);
+    }
+    return false;
+}
+
 int main(){
     int arr[]={-3,2,1,4};
     int n= sizeof(arr)/sizeof(arr[0]);
     cout<<bruteforce(arr,n);
-
+    optimumApproch(arr,n);
 }

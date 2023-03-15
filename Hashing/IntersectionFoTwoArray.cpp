fb14a62ebfd8371d42f79f051310954c8ee55d84
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-03-15 23:43:11 +0530

    hashing 2

--- Hashing/IntersectionFoTwoArray.cpp
+++ Hashing/IntersectionFoTwoArray.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
     int arr1[]={10,10,15};
     int arr2[]={10,10,15};
-    unordered_set<int>s;
-    for(int i=0; i<4; i++){
-        s.insert(arr1[i]);
-    }
+    unordered_set<int>s(arr1, arr1+3);
+    // for(int i=0; i<4; i++){
+    //     s.insert(arr1[i]);
+    // }
     for(int j=0; j<3; j++){
         if(s.find(arr2[j])!=s.end()){
             cout<<arr1[j]<<" ";
-            s.erase(arr2[j]);
+            s.erase(arr1[j]);
         }
     }
 }
--- Hashing/UnionOfTwoArray.cpp
+++ Hashing/UnionOfTwoArray.cpp
@@ -7,11 +7,10 @@ int main(){
     int arr1[]={1,2,3,4,5,1};
     int arr2[]={1,2,3,6};
 
-    unordered_set<int>s;
-
-    for(int i=0; i<5; i++){
-        s.insert(arr1[i]);
-    }
+    unordered_set<int>s(arr1, arr1+6);
+    // for(int i=0; i<5; i++){
+    //     s.insert(arr1[i]);
+    // }
     for(int j=0; j<4; j++){
         s.insert(arr2[j]);
     }

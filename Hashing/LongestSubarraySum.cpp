0c86230802b6736bb9c025f08d6441291e143e33
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-03-16 12:25:45 +0530

    longest subarray sum optimum

--- Hashing/LongestSubarraySum.cpp
+++ Hashing/LongestSubarraySum.cpp
@@ -25,14 +25,33 @@ int BruteForce(int arr[], int sum, int n){
     return res;
 }
 
-int main(){
-    // test case1
-    // int arr[] = {5,8,-4,-4,9,-2,2};
-    // int sum=0;
+int Optimum_res(int arr[], int sum, int n){
+    int current_sum=0,res=0;
+    unordered_map<int, int> m;
+    for(int i=0; i<n; i++){
+        current_sum+=arr[i];
+        if(current_sum==sum){
+            res=i+1;
+        }
+        if(m.find(current_sum)==m.end()){
+            m.insert({arr[i],i});
+        }
+        if(m.find(current_sum-sum)!=m.end()){
+            res=max(res, i-m[current_sum-sum]);
+        }
+    }
+    return res;
+}
+
+int main()  
+    // test case1 ans should be 3
+    int arr[] = {5,8,-4,-4,9,-2,2};
+    int sum=0;
 
     //test case2
-    int arr[] = {3,1,0,1,8,2,3,6};
-    int sum=5;
+    // int arr[] = {3,1,0,1,8,2,3,6};
+    // int sum=5;
     int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<BruteForce(arr,sum,n);
+    cout<<BruteForce(arr,sum,n)<<endl;
+    cout<<Optimum_res(arr,sum,n)<<endl;
 }

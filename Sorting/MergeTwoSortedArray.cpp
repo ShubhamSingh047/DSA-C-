e909ee4eb7e98b1ee6ae870f5efb0ffdb95eaa32
Shubham Singh <shubham.shubham.singh13@gmail.com>
2023-03-28 01:41:54 +0530

    merge sort

---
+++ Sorting/MergeTwoSortedArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void MergeSort(int arr1[], int arr2[], int n1, int n2){
+    int i=0, j=0;
+    while(i<n1 && j<n2){
+        if(arr1[i]<=arr2[j]){
+            cout<<arr1[i]<<" ";
+            i++;
+        }else{
+            cout<<arr2[j]<<" ";
+            j++;
+        }
+    }
+    while(i<n1){
+        cout<<arr1[i]<<" ";
+        i++;
+    }
+    while(j<n2){
+        cout<<arr2[j]<<" ";
+        j++;
+    }
+}
+
+int main(){
+    int arr1[]={10,20,30,40};
+    int arr2[]={1,1,2};
+    int n1=sizeof(arr1)/sizeof(arr1[0]);
+    int n2=sizeof(arr2)/sizeof(arr2[0]);
+    MergeSort(arr1,arr2,n1,n2);
+}
